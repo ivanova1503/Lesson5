1c96731103bc8146b1b65ff9d9c7f225337fd150
ivanova1503 <ivanova1503@gmail.com>
2022-01-07 10:29:18 +0200

    Подправлена домашняя работа 5

--- src/test/java/dex/ivanova/lesson5/StudentsRegistrationFormTests.java
+++ src/test/java/dex/ivanova/lesson5/StudentsRegistrationFormTests.java
@@ -27,11 +27,11 @@ public class StudentsRegistrationFormTests {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
 
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Egorov");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#genterWrapper").$(byText("Other")).click();
-        $("#userNumber").setValue("1231231230");
+        $("#firstName").setValue("Yuliya");
+        $("#lastName").setValue("Ivanova");
+        $("#userEmail").setValue("test@test.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("7777703333");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("2008");
@@ -39,7 +39,7 @@ public class StudentsRegistrationFormTests {
         $("#subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#currentAddress").setValue("Some address 1");
+        $("#currentAddress").setValue("Tiraspol");
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
@@ -48,6 +48,17 @@ public class StudentsRegistrationFormTests {
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").$(byText("Student Name"))
-                .parent().shouldHave(text("Alex Egorov"));
+                .parent().shouldHave(text("Yuliya Ivanova"));
+
+        $(".table-responsive").shouldHave(text("Yuliya"));
+        $(".table-responsive").shouldHave(text("Ivanova"));
+        $(".table-responsive").shouldHave(text("test@test.com"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("7777703333"));
+        $(".table-responsive").shouldHave(text("30 July,2008"));
+        $(".table-responsive").shouldHave(text("Math"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("1.png"));
+        $(".table-responsive").shouldHave(text("NCR Noida"));
     }
 }
