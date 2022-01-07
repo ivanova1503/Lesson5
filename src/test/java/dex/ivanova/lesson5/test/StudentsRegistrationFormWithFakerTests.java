a7fcf13ead8812bffed835b311893c308fb4d36b
ivanova1503 <ivanova1503@gmail.com>
2022-01-07 14:14:09 +0200

    added faker

---
+++ src/test/java/dex/ivanova/lesson5/test/StudentsRegistrationFormWithFakerTests.java
@@ -0,0 +1,65 @@
+package dex.ivanova.lesson5.test;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import com.github.javafaker.Faker;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static dex.ivanova.lesson5.utils.RandomUtils.getRandomEmail;
+import static dex.ivanova.lesson5.utils.RandomUtils.getRandomString;
+
+public class StudentsRegistrationFormWithFakerTests {
+
+    Faker faker = new Faker();
+
+    //String userEmail = getRandomEmail();
+    //String firstName = getRandomString(12);
+    String firstName= faker.name().firstName();
+    String userEmail = faker.internet().emailAddress();
+    String currentAddress = faker.lebowski().quote() + " " + faker.medical().symptoms();
+
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue("Ivanova");
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("7777703333");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $("[aria-label$='July 30th, 2008']").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text(firstName+" Ivanova"));
+
+        $(".table-responsive").$(byText("Student Email"))
+                .parent().shouldHave(text(userEmail));
+    }
+}
