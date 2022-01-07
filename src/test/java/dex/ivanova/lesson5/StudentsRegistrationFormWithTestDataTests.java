8466840c69b4ca45b2a194636d0b65e6de2a13e6
ivanova1503 <ivanova1503@gmail.com>
2022-01-07 12:44:45 +0200

    added TestData

---
+++ src/test/java/dex/ivanova/lesson5/StudentsRegistrationFormWithTestDataTests.java
@@ -0,0 +1,60 @@
+package dex.ivanova.lesson5;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static dex.ivanova.lesson5.test.TestData.userEmail;
+
+public class StudentsRegistrationFormWithTestDataTests {
+
+    String firstName= "Yuliya",
+            lastName= "Ivanova";
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+
+    @Test
+    void fillFormTest() {
+
+
+
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("7777703333");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $("[aria-label$='July 30th, 2008']").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Tiraspol");
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text(firstName + " " +lastName));
+
+
+    }
+}
---
+++ src/test/java/dex/ivanova/lesson5/test/TestData.java
@@ -0,0 +1,6 @@
+package dex.ivanova.lesson5.test;
+
+public class TestData {
+
+    static String userEmail ="test@test.com";
+}
